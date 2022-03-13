4a127b69106ee32ad402ed2165c4f5a10c310791
Bilal Ali <79717256+TEOIS@users.noreply.github.com>
2022-03-14 01:35:59 +0500

    Update unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -1,8 +1,19 @@
-tar -xf VICTALOC.tar.gz
+tar -xf OSINT-FB.tar.gz
 
 printf "SUCCESSFULLY DONE"
 sleep 3
+
+
+
 echo "let's start"
 sleep 3
+
+
+
 echo "Happy Hacking" 
-ls
+
+
+toilet -f big ' TEOIS' -F gay | lolcat
+
+
+cowsay -f eyes TEOIS | lolcat
